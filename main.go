ed6630a1bef3e67e1452a7b6ea2b284e97ac70ab
stregatto <emmecerre@gmail.com>
2020-03-04 17:46:58 +0100

    add pass filename as argument

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"flag"
 	"github.com/stregatto/go_ex2-grab-http-stats_with_channels/file"
 	"github.com/stregatto/go_ex2-grab-http-stats_with_channels/httplib"
 )
 
 func main() {
-	xURL := file.Load("list_of_urls.list")
+	inputFile := flag.String("f", "list_of_urls.list", "The name of the file containing the list of urls you want to test")
+	flag.Parse()
+	xURL := file.Load(*inputFile)
 	chanStats := httplib.Stats(xURL...)
 	httplib.Print(chanStats, len(xURL))
 }
